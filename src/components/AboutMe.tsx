b7aa637f7b04a629b0af04d1760176def1a0458b
cantti <cantti@outlook.com>
2025-03-23 11:05:04 +0100

    feat: update aboutme

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,17 +1,33 @@
-import _ from "lodash";
-import { discography } from "../data/discography";
+import _ from 'lodash';
 
 export const AboutMe = () => {
+  const canttiReleases = [
+    'track=379330236',
+    'track=3883024499',
+    'album=2599868140',
+    'track=39885008',
+    'track=350652303',
+    'album=966016900',
+    'album=4019982752',
+    'album=1707757818',
+    'album=2793997991',
+    'album=3146788245',
+    'album=1913466361',
+    'album=717509761',
+    'album=3122310838',
+  ];
+  const isReleases = ['album=92237236', 'album=1124706077', 'album=2077738303'];
   return (
     <>
       <h3>About me</h3>
       <p>
-        Musician and software developer currently living in Sweden. I mainly
-        work in the reggae dub genre under the pseudonym <b>Cantti</b>. There
-        are also releases in the Synthwave genre under the pseudonym{" "}
-        <b>Imminent State</b>. Below is a list of my albums with links to
-        Bandcamp where you can buy them. All albums are also available on
-        streaming services. Recently I started my{" "}
+        Musician and software developer. I mainly work in the reggae dub genre
+        under the pseudonym <b>Cantti</b>. There are also releases in the
+        Synthwave genre under the pseudonym <b>Imminent State</b>. Below is a
+        list of my releases with the links to Spotify and Bandcamp. All releases
+        can be found on other streaming platforms.
+      </p>
+      <p>
         <a
           href="https://www.youtube.com/channel/UCS26LO1Y8E8ACgkgv7sOd7A"
           rel="noreferrer"
@@ -21,34 +37,44 @@ export const AboutMe = () => {
         </a>
         .
       </p>
-      <p>Below is a partial list of my music releases.</p>
-      <ul>
-        {_.orderBy(discography, (x) => x.date, ["desc"]).map((album, index) => (
-          <li key={index}>
-            <div>
-              {album.artist} - {album.title} ({album.date.getFullYear()})
-            </div>
-            <div className="fw-light">{album.genre}</div>
-            <div>
-              {album.listenLink && (
-                <a
-                  href={album.listenLink}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="me-2"
-                >
-                  Listen
-                </a>
-              )}
-              {album.buyLink && (
-                <a href={album.buyLink} target="_blank" rel="noreferrer">
-                  Buy
-                </a>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
+      <h4>Synthwave releases (Imminent state)</h4>
+      <h5>Spotify</h5>
+      <iframe
+        src="https://open.spotify.com/embed/artist/2XacwfKxItLK7VXXS1r8wR?utm_source=generator&theme=0"
+        width="100%"
+        height="152"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <h5>Bandcamp</h5>
+      {isReleases.map((rel) => (
+        <iframe
+          key={rel}
+          src={`https://bandcamp.com/EmbeddedPlayer/${rel}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/`}
+          seamless
+          className="me-4 mb-4 border-0"
+          style={{ width: '400px', height: '350px' }}
+        />
+      ))}
+      <h4>Reggae releases (Cantti)</h4>
+      <h5>Spotify</h5>
+      <iframe
+        src="https://open.spotify.com/embed/artist/0ESGrsq25JWPHp4TRfCltL?utm_source=generator&theme=0"
+        width="100%"
+        height="152"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <h5>Bandcamp</h5>
+      {canttiReleases.map((rel) => (
+        <iframe
+          key={rel}
+          src={`https://bandcamp.com/EmbeddedPlayer/${rel}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/`}
+          seamless
+          className="me-4 mb-4 border-0"
+          style={{ width: '400px', height: '350px' }}
+        />
+      ))}
     </>
   );
 };
